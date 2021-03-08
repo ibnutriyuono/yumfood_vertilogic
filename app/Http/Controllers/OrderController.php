b18625d2b5bdb10a1c747b43c9fe5366f57dfea4
ibnutriyuono <ibnutriyuono@gmail.com>
2021-03-08 19:53:48 +0700

    add: store order method

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers;
 
 use App\Order;
 use Illuminate\Http\Request;
+use App\Http\Resources\OrderResource;
+use App\Http\Requests\OrderRequest;
+use App\Dish;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class OrderController extends Controller
 {
@@ -15,16 +19,7 @@ class OrderController extends Controller
     public function index()
     {
         //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+        return OrderResource::collection(Order::orderBy('id', 'asc')->paginate());
     }
 
     /**
@@ -33,9 +28,32 @@ class OrderController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(OrderRequest $request)
     {
         //
+        try {
+            $price = Dish::findOrFail($request['dish_id'])['price'];
+            $amount = $price * $request['quantity'];
+            $validated = $request->validated();
+            $order = new Order;
+            $order->vendor_id = $request['vendor_id'];
+            $order->dish_id = $request['dish_id'];
+            $order->user_id = $request['user_id'];
+            $order->quantity = $request['quantity'];
+            $order->amount = $price * $request['quantity'];
+            $sucessAddData = $order->save();
+            if ($sucessAddData) {
+                return response()->json([
+                    "statusCode" => 200,
+                    "message" => "Success. Order number $order[id] successfully added."
+                ]);
+            }
+        } catch (ModelNotFoundException $e) {
+            return response()->json([
+                "statusCode" => 404,
+                "message" => "Sorry. Data not found"
+            ], 404);
+        }
     }
 
     /**
@@ -45,17 +63,6 @@ class OrderController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function show(Order $order)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Order  $order
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Order $order)
     {
         //
     }
