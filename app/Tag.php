3e2686a904902b15a2021d248ace3ae080c9ad4a
ibnutriyuono <ibnutriyuono@gmail.com>
2021-03-08 10:26:33 +0700

    add: fillable name column

--- app/Tag.php
+++ app/Tag.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tag extends Model
 {
+    protected $fillable = ['name'];
+    
     public function vendors()
     {
         return $this->morphedByMany('App\Vendor', 'taggable');
